4f234bba8dcb50cf6b508ab13e24ac85e82f7bfd
Yonggang Luo <luoyonggang@gmail.com>
2014-12-18 11:17:11 +0800

    Build bower.

--- build-deps.bat
+++ build-deps.bat
@@ -1,7 +1,7 @@
 set PATH=C:\CI-Tools\vcs\git\cmd;%PATH%
 call npm install -g grunt-cli
 call npm install -g vulcanize
-call npm install -g 
+call npm install -g bower
 call npm install
 call bower update
 pause
